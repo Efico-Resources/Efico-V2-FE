6525dd9e16406aa00fe4c863f9c82bd4cc84d78e
Oloyede Oluwatobi <oloyedeoluwatobi52@gmail.com>
2021-07-22 20:14:32 +0100

    contact page

--- efico-FAQs/script.js
+++ efico-FAQs/script.js
@@ -1,18 +1,1 @@
 
-const myPage = document.querySelector('ul');
-function page(total, num){
-	let myList = '';
-	let activePage;
-	let firstPage = num;
-	let nextPage = num +1;
-
-	for (let i = num; i <= total; i++) {
-		 if (num == i) {
-		 	activePage = "active"; 
-		 }
-		 else{
-		 	activePage = ""
-		 }
-myList =+ `<li class=${activePage}>${i}</li>`;
-	}
-}
